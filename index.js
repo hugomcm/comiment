8f4c5f236a5cd07e5b15555e824e193c86c33c13
Hugo Monteiro <hugomcm@gmail.com>
2020-03-06 21:01:10 +0000

    1st attempt to set the index to be used as a module

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const { buildInstance } = require('./utils/factory');
-const { openTick, closeTick, printTick } = require('./functionality');
 
-const agent1 = buildInstance('agent1', { openTick, closeTick, printTick });
+function getFunctionality(path = './functionality') {
+  return require(path);
+}
 
-agent1.openTick(123123);
-agent1.printTick();
-agent1.openTick(523423);
-agent1.printTick();
-agent1.closeTick(1.92319);
-agent1.printTick();
-agent1.closeTick(1.92314);
-agent1.printTick();
-agent1.openTick(1.32367);
+module.exports = { buildInstance, getFunctionality };
