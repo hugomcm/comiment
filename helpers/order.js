a4265ef4e6538cc34eb23a7c395543758dafa727
Hugo Monteiro <hugomcm@gmail.com>
2020-03-18 16:41:28 +0000

    remove garbage

--- helpers/order.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  init: state => order => ({ newState: { ...state, order } }),
-  open: state => () => console.log('open order'),
-  close: state => () => console.log('close order'),
-  print: state => () => console.log('state', state)
-};
--- helpers/tick.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  open: state => ask => ({ newState: { ...state, ask } }),
-  close: state => bid => ({ newState: { ...state, bid } }),
-  print: state => () => {
-    console.log('state', state);
-    console.log('-------------------------------------');
-  }
-};
